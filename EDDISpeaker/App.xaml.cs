b7a5695dfec64a100dbc6a6be8cd16775447e271
Lazialize <mistio100@gmail.com>
2021-08-05 12:39:27 +0900

    Prism 使ってブランクプロジェクト状態にした

--- EDDISpeaker/App.xaml.cs
+++ EDDISpeaker/App.xaml.cs
@@ -5,13 +5,24 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using EDDISpeaker.Views;
+using Prism.Ioc;
+using Prism.Unity;
 
 namespace EDDISpeaker
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
     }
 }
