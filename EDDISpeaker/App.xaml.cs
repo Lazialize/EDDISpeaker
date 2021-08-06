f4ed17754eb3914e66555ba2aded1bec03619b51
Lazialize <mistio100@gmail.com>
2021-08-06 15:17:29 +0900

    とりあえず初期設定の処理を書いた。
    ここに書くの間違ってる気がするので、とりあえず動くのができたら後で直す。

--- EDDISpeaker/App.xaml.cs
+++ EDDISpeaker/App.xaml.cs
@@ -8,8 +8,10 @@ using System.Threading.Tasks;
 using System.Windows;
 using EDDISpeaker.Utils;
 using EDDISpeaker.Views;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using Prism.Ioc;
 using Prism.Unity;
+using Injector;
 
 namespace EDDISpeaker
 {
@@ -22,6 +24,26 @@ namespace EDDISpeaker
 
         public App() {
             _config = ConfigUtils.Load<Config>("general");
+
+            if (_config.VoiceroidSettings.InstallationPath == null) {
+                MessageBox.Show("Voiceroid2のインストール先を設定してください。");
+                using var dialog = new CommonOpenFileDialog() { 
+                    Title = "フォルダを選択してください",
+                    IsFolderPicker = true
+                };
+
+                dialog.ShowDialog();
+                _config.VoiceroidSettings.InstallationPath = dialog.FileName;
+            }
+
+            if (_config.VoiceroidSettings.AuthenticationCode == null) {
+                MessageBox.Show("Voiceroid2の認証トークンが設定されていません。\n一度Voiceroid2を起動してからOKを押してください。");
+                var code = Injector.Injector.GetKey("VoiceroidEditor");
+                if (code != null) {
+                    MessageBox.Show($"認証コード{code}を取得しました。\nVoiceroid2を終了してOKを押してください。");
+                }
+                _config.VoiceroidSettings.AuthenticationCode = code;
+            }
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
