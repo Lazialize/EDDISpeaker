b193ff8887f234cf87047757ffea4e4b4b705a56
Lazialize <mistio100@gmail.com>
2021-08-06 13:45:52 +0900

    設定ファイルの読込と保存やるぞやるぞ。
    クソ適当なので後で直します…。

--- EDDISpeaker/App.xaml.cs
+++ EDDISpeaker/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Windows;
+using EDDISpeaker.Utils;
 using EDDISpeaker.Views;
 using Prism.Ioc;
 using Prism.Unity;
@@ -16,13 +18,23 @@ namespace EDDISpeaker
     /// </summary>
     public partial class App : PrismApplication
     {
+        private Config _config;
+
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            _config = ConfigUtils.Load<Config>("general");
+            containerRegistry.RegisterInstance(_config);
         }
 
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            ConfigUtils.Save(_config, "general");
+            base.OnExit(e);
         }
     }
 }
