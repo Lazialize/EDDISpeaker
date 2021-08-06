ae1536910a4a8128f6c9aaa63b0d39e210d6d4c3
Lazialize <mistio100@gmail.com>
2021-08-06 14:58:22 +0900

    dll にインジェクションするやつ。認証コードを引っ張ってくるだけ。

---
+++ Injector/Injector.cs
@@ -0,0 +1,51 @@
+﻿using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace Injector
+{
+    class Injector
+    {
+        public static string GetKey(string processName)
+        {
+            var processes = Process.GetProcessesByName(processName);
+            if (processes.Length == 0) return null;
+
+            var process = processes[0];
+
+            var app = new WindowsAppFriend(process);
+            WindowsAppExpander.LoadAssembly(app, typeof(Injector).Assembly);
+            dynamic injected = app.Type(typeof(Injector));
+
+            try
+            {
+                return injected.GetKeyFromInjectedProcess();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static string GetKeyFromInjectedProcess()
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                if (assembly.GetName().Name == "AI.Framework.App")
+                {
+                    var type = assembly.GetType("AI.Framework.AppFramework");
+                    var property = type.GetProperty("Current");
+                    dynamic current = property.GetValue(type);
+                    return (string)current.AppSettings.LicenseKey;
+                }
+            }
+
+            return null;
+        }
+    }
+}
