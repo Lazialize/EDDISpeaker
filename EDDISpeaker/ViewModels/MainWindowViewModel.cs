c27dca61bb2e9d82326726e4e4e3530cab11ce88
Lazialize <mistio100@gmail.com>
2021-08-08 13:04:53 +0900

    適当UI。デザインセンスねぇ～。

--- EDDISpeaker/ViewModels/MainWindowViewModel.cs
+++ EDDISpeaker/ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,14 @@ namespace EDDISpeaker.ViewModels
 {
     class MainWindowViewModel: BindableBase
     {
+        private readonly Config _config;
+
+        public MainWindowViewModel(Config config)
+        {
+            _config = config;
+        }
+
+        public string AppName => AppInfo.AppName;
+        public string Version => AppInfo.Version;
     }
 }
