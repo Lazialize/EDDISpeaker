79ae87235d3cd21a4be485fdd88733563cdade75
Lazialize <mistio100@gmail.com>
2021-08-08 11:55:03 +0900

    アプリケーション情報用のクラス作っちゃおw

---
+++ EDDISpeaker/AppInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDDISpeaker
+{
+    public static class AppInfo
+    {
+        public const string AppName = "EDDI Speaker";
+        public const string Version = "1.0.0";
+    }
+}
